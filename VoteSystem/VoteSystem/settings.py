17d6510021b5f1f9629bf36b0140a3d007ab52f0
daeni-dang <de021@naver.com>
2021-11-09 01:03:54 +0900

    fix

--- VoteSystem/VoteSystem/settings.py
+++ VoteSystem/VoteSystem/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'VOTE',
         'USER' : 'root',
-        'PASSWORD': 'leesieun8018',
+        'PASSWORD': '1234',
         'HOST': 'localhost',
         'PORT': '3306'
     }
