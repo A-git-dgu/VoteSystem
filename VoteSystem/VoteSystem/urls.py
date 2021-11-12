f46135c9ee2a947ad399268b18eef70795863b7b
EunSeo <bnsy33@naver.com>
2021-11-12 15:48:47 +0900

    return

--- VoteSystem/VoteSystem/urls.py
+++ VoteSystem/VoteSystem/urls.py
@@ -19,6 +19,5 @@ from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('second/', TemplateView.as_view(template_name='second.html')),
     re_path('.*', TemplateView.as_view(template_name='index.html')),
 ]
