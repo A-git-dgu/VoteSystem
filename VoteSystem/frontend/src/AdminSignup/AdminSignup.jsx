b13bc10494be8aa88539421fe3e195f0a73d5a02
kimgaeun21 <kimgaeun21@naver.com>
2021-11-16 21:47:05 +0900

    Adminsignup-screen size, add admin info

--- VoteSystem/frontend/src/AdminSignup/AdminSignup.jsx
+++ VoteSystem/frontend/src/AdminSignup/AdminSignup.jsx
@@ -1,3 +1,4 @@
+
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import NativeSelect from '@mui/material/NativeSelect';
@@ -6,6 +7,7 @@ import Input from '@mui/material/Input';
 import DateRangePicker from '@mui/lab/DateRangePicker';
 import AdapterDateFns from '@mui/lab/AdapterDateFns';
 import LocalizationProvider from '@mui/lab/LocalizationProvider';
+import FormControl from '@mui/material/FormControl';
 import Box from '@mui/material/Box';
 import Nav from '../Main/main';
 
@@ -17,20 +19,30 @@ function Adminsignup() {
     return (
     <>
     <Nav/>
-    <h3 id="title">후보자 등록</h3>
+      <div id="outer_form">
+      <h1>관리자 선거 개설</h1>
         <form action="" method="post">
-            <table id="table" width="100%">
+            <table id="table" width="100%" >
                 <tr>
                     <td width="50%" id="right_td">
                         <h2 id="right_td"> 선거 정보 입력</h2>
-                        <p>선거 이름 <Input id="election_name" placeholder="선거 이름을 입력하세요." />
+                    </td>
+                    <td width="50%" id="right_td">
+                        <h2 id="right_td"> 관리자 정보 입력</h2>
+                    </td>
+                </tr>
+                <tr>
+                    <td width="50%" id="right_td">
+                        <p>선거 이름&nbsp;&nbsp;<Input id="election_name" placeholder="선거 이름을 입력하세요." />
                         <Button size="small" variant="contained">중복확인</Button></p>
-                        <p>선거 선택
+                        <p>선거 선택&nbsp;&nbsp;
                             <NativeSelect default="select_election">
                                 <option value={"select_election"}>-- 선거 선택 --</option>
+                                <option value={"select_election"}>  찬반 투표  </option>
+                                <option value={"select_election"}>  후보자 투표  </option>
                             </NativeSelect>
                          </p>
-                        <p>소속 기관 <Input id="institution" placeholder="소속 기관을 입력하세요" /></p>
+                        <p>소속 기관 &nbsp;&nbsp;<Input id="institution" placeholder="소속 기관을 입력하세요" /></p>
                         <p>후보자 등록 기간</p>
                          <LocalizationProvider dateAdapter={AdapterDateFns}>
                          <DateRangePicker
@@ -69,21 +81,38 @@ function Adminsignup() {
                      </LocalizationProvider>
                      </td>
                 <td width="50%" id="right_td">
-                <h2 id="right_td"> 관리자 정보 입력</h2>
-                    <p>관리자명<Input id="admin_name" placeholder="이름을 입력하세요." /></p>
-                    <p>관리자 e-mail<Input id="email" placeholder="dongguk@dgu.kr" /></p>
-                    <p>관리자 ID<Input id="admin_id" placeholder="아이디를 입력하세요(영어 대소문자 구분)" />
+                    <p>ID&nbsp;&nbsp;<Input id="admin_id" placeholder="아이디를 입력하세요(영어 대소문자 구분)" />
                      <Button size="small" variant="contained">중복확인</Button></p>
-                    <p>비밀번호<Input id="pwd" placeholder="알파벳+숫자 (영어 대소문자 구분)" /></p>
-                    <p>비밀번호 재확인<Input placeholder="" helperText="1000자 이내로 작성하세요."/></p>
+                    <p>비밀번호&nbsp;&nbsp;<Input id="pwd" placeholder="알파벳+숫자 (영어 대소문자 구분)" /></p>
+                    <p>비밀번호 재확인&nbsp;&nbsp;<Input placeholder="" helperText="1000자 이내로 작성하세요."/></p>
+                    <p>이름&nbsp;&nbsp;<Input id="admin_name" placeholder="이름을 입력하세요." /></p>
+                    <p>주민등록번호 &nbsp;&nbsp;
+                    <Input className="admin_ssn" placeholder="000000" id="fssn"/>
+                    &nbsp;&nbsp;- <Input className="admin_ssn" type="password" placeholder="1234567" id="lssn"/>
+                    <button className="checkInput_signup" id="checkInput_signup">본인확인</button></p>
+                    <p> 핸드폰 번호 &nbsp;&nbsp;
+                    <Input className="admin_ssn" placeholder="010" id="fssn"/>&nbsp;&nbsp;-
+                    <Input className="admin_ssn" placeholder="1234" id="fssn"/>&nbsp;&nbsp;-
+                    <Input className="admin_ssn" placeholder="1234" id="fssn"/></p>
+                    <p>e-mail&nbsp;&nbsp;<Input id="email" placeholder="dongguk@dgu.kr" /></p>
+                    <p>주소 &nbsp;&nbsp;<TextField multiline cols="22px" placeholder="주소를 입력하세요." id="address" className="input_form"/></p>
 
                 </td>
           </tr>
+          <tr>
+            <td>
+             <div id="bottom_button">
+
+                </div>
+            </td>
+          </tr>
         </table>
+      </form>
         <div id="bottom_button">
         <Button variant="contained" type="submit">선거 개설</Button>
         </div>
-      </form>
+     </div>
+
     </>
   );
 }
