b3afdd1ba5141a9853c0e55f91d5a798c1073b0f
kimgaeun21 <kimgaeun21@naver.com>
2021-11-27 23:09:32 +0900

    web3

--- VoteSystem/frontend-blockchain/client/src/MainAdmin/mainAdmin.jsx
+++ VoteSystem/frontend-blockchain/client/src/MainAdmin/mainAdmin.jsx
@@ -107,46 +107,46 @@ export default function MainAdmin() {
                 <div id="container">
 
                     <p id="title_mainadmin">선거 정보
-                        <button className="signupPage_Button" id="requestSignup_admain"> 정보 수정 </button>
+                        <button className="mainadmin_Page_Button" id="requestSignup_admain"> 정보 수정 </button>
                     </p>
                     <div id="form_border_mainadmin">
                         <div id="left_form_mainadmin">
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">선거 이름 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">선거 이름 : </div>
                                 {modifyMode==false && <Input value = {election.election_name} id="election_name" className="input_form_mainadmin"/>}
                                 {modifyMode==true && <Input placeholder = {election.election_name} id="election_name" className="input_form_mainadmin"/>}
                             </div>
-                            <div className="each_form_signup_admain">
-                            <div className="article_signup_admain">선거 종류 : </div>
+                            <div className="each_form_mainadmin">
+                            <div className="article_mainadmin">선거 종류 : </div>
                                 <NativeSelect value={election.election_type} className="input_form_mainadmin">
                                     <option id="TF" value={0}>  찬반 투표  </option>
                                     <option id="who" value={1}>  후보자 투표  </option>
                                 </NativeSelect>
                             </div>
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">선거 번호 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">선거 번호 : </div>
                                 <div className="election_num_view">&nbsp; {election.election_num}</div>
                             </div>
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">소속 기관 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">소속 기관 : </div>
                                 {modifyMode==false && <Input value = {election.institution} id="institution" className="input_form_mainadmin"/>}
                                 {modifyMode==true && <Input placeholder = {election.institution} id="institution" className="input_form_mainadmin"/>}
                             </div>
                         </div>
                         <div id="middle_line_mainadmin"></div>
                         <div id="right_form_mainadmin">
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">회원 이름 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">회원 이름 : </div>
                                 {modifyMode==false && <Input value = {election.admin_name} id="admin_name" className="input_form_mainadmin"/>}
                                 {modifyMode==true && <Input placeholder = {election.admin_name} id="admin_name" className="input_form_mainadmin"/>}
                             </div>
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">관리자 e-mail : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">관리자 e-mail : </div>
                                 {modifyMode==false && <Input type="email" value = {election.admin_email} className="input_form_mainadmin" id="admin_email"/>}
                                 {modifyMode==true && <Input type="email" placeholder = {election.admin_email} className="input_form_mainadmin" id="admin_email"/>}
                             </div>
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">선거 기간 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">선거 기간 : </div>
                                 <div className="input_form_mainadmin2">
                                     <LocalizationProvider dateAdapter={AdapterDateFns}>
                                         <DateRangePicker
@@ -167,8 +167,8 @@ export default function MainAdmin() {
                                     </LocalizationProvider>
                                 </div>
                             </div>
-                            <div className="each_form_signup_admain">
-                                <div className="article_signup_admain">후보자 등록기간 : </div>
+                            <div className="each_form_mainadmin">
+                                <div className="article_mainadmin">후보자 등록기간 : </div>
                                 <div className="input_form_mainadmin2">
                                     <LocalizationProvider dateAdapter={AdapterDateFns}>
                                         <DateRangePicker
@@ -211,9 +211,9 @@ export default function MainAdmin() {
                     </div>
                     <div id="button_site">
                         { (election.election_type==-1 || election.election_type==2) ?
-                            <Link to={'/electionResultAdmin/'+election.election_num}><button id="result_mainAdmin" className="signupPage_Button">결과보기</button></Link>
-                            : <button id="result_mainAdmin" className="signupPage_Button" onClick={ballotCount}>개표하기</button> }
-                        <button id="delete_election" className="signupPage_Button" onClick={handleClickOpen}>선거삭제</button>
+                            <Link to={'/electionResultAdmin/'+election.election_num}><button id="result_mainAdmin" className="mainadmin_Page_Buttondmin_Page_Button">결과보기</button></Link>
+                            : <button id="result_mainAdmin" className="mainadmin_Page_Button" onClick={ballotCount}>개표하기</button> }
+                        <button id="delete_election" className="mainadmin_Page_Button" onClick={handleClickOpen}>선거삭제</button>
                         <Dialog
                             fullScreen={fullScreen}
                             open={open}
@@ -229,10 +229,10 @@ export default function MainAdmin() {
                             </DialogContentText>
                         </DialogContent>
                         <DialogActions>
-                            <Button autoFocus onClick={handleClose} className="signupPage_Button">
+                            <Button autoFocus onClick={handleClose} className="mainadmin_Page_Button">
                                 취소
                             </Button>
-                            <Button onClick={deleteElection} color="error" className="signupPage_Button" >
+                            <Button onClick={deleteElection} color="error" className="mainadmin_Page_Button" >
                                 선거 삭제
                             </Button>
                         </DialogActions>
