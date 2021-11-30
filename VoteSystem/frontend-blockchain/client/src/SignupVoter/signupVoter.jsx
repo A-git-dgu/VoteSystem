e5d1503ef28226b86afea535b1fa164498bbace7
EunSeo <bnsy33@naver.com>
2021-11-30 15:16:20 +0900

    Add check SSN.

--- VoteSystem/frontend-blockchain/client/src/SignupVoter/signupVoter.jsx
+++ VoteSystem/frontend-blockchain/client/src/SignupVoter/signupVoter.jsx
@@ -118,6 +118,27 @@ export default function SignupVoter() {
          })
     };
 
+    function checkSSN(){
+        const fssnElement = document.getElementById("fssn")
+        const lssnElement = document.getElementById("lssn")
+        const fssn = fssnElement.value
+        const lssn = lssnElement.value
+        if(fssn.length!=6 || lssn.length!=7 || fssn[2]+fssn[3] > 12 || fssn[4]+fssn[5] > 31 || fssn[4]+fssn[5]==0){
+            alert("유효한 주민등록번호를 입력하세요.")
+        }
+        else if(isNaN(fssn)||isNaN(lssn)){
+            alert("유효한 주민등록번호를 입력하세요.")
+        }
+        else if(!(lssn[0]==5)&&!(fssn[0]+fssn[1]>21&&(lssn[0]==1||lssn[0]==2)) && !(fssn[0]+fssn[1]<=21&&(lssn[0]==3||lssn[0]==4))){
+            alert("유효한 주민등록번호를 입력하세요.")
+        }
+        else{
+            alert("유요한 주민등록번호입니다!")
+            fssnElement.disabled="true"
+            lssnElement.disabled="true"
+        }
+    }
+
     return (
         <>
             <Nav Type={"Voter"}/>
@@ -152,7 +173,7 @@ export default function SignupVoter() {
                             <div className="article_signup" id="backmargin_article_signup">주민등록번호</div>
                             <Input className="ssn_signup" placeholder="000000" id="fssn"/>
                             &nbsp;&nbsp;- <Input className="ssn_signup" type="password" placeholder="1234567" id="lssn"/>
-                            <button className="signupPage_Button" id="checkInput_signup">본인확인</button>
+                            <button className="signupPage_Button" id="checkInput_signup" onClick={checkSSN}>본인확인</button>
                         </div>
                     </div>
                     <div id="middle_line_signup"></div>
